3991294fc1dea543ed3c440fc74bf3b9345efe93
dongil618 <rlaehddlf618@gmail.com>
2022-07-29 20:19:08 +0900

    프로젝트 생성

---
+++ src/main/java/com/hanghae99/airbnbclonebe/AirbnbCloneBeApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae99.airbnbclonebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnbCloneBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirbnbCloneBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanghae99/airbnbclonebe/AirbnbCloneBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae99.airbnbclonebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbCloneBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
